3672f42d9e3cf139812aeed0ee7f6fc53c652304
Nam Le <nam.leduc02@gmail.com>
2020-01-20 09:54:50 +0100

    change base tile

--- src/components/LeafletPanel.tsx
+++ src/components/LeafletPanel.tsx
@@ -50,7 +50,7 @@ export class LeafletPanel extends PureComponent<Props, MapState> {
     console.log('data....', this.props.data);
 
     const openStreetMap = L.tileLayer(
-      'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
       {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
